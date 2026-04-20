70749ee56efddea10275274072745d6ce4a32e01
masamatsu0205 <masamatsu0205@gmail.com>
2026-04-20 18:20:30 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,3 @@
 <?php
-
-try {
-    $pdo = new PDO(
-        "mysql:host=mysql.railway.internal;port=3306;dbname=railway;charset=utf8mb4",
-        "root",
-        "jLxvSNdaYyBxdqIQPuFVzlZxrwnVeClu",
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_TIMEOUT => 3   // ←これ超重要（固まり防止）
-        ]
-    );
-} catch (Exception $e) {
-    die("DB接続エラー: " . $e->getMessage());
-}
-
+echo "PHPは動いてる";
 ?>
