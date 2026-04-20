d22704f501088210883ab2ea0aaf8404cf4f0a45
masamatsu0205 <masamatsu0205@gmail.com>
2026-04-20 18:18:28 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,18 @@
 <?php
-// データベース接続
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=calendar_db;charset=utf8",
-    "root",
-    "",
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
+
+try {
+    $pdo = new PDO(
+        "mysql:host=mysql.railway.internal;port=3306;dbname=railway;charset=utf8mb4",
+        "root",
+        "jLxvSNdaYyBxdqIQPuFVzlZxrwnVeClu",
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_TIMEOUT => 3   // ←これ超重要（固まり防止）
+        ]
+    );
+} catch (Exception $e) {
+    die("DB接続エラー: " . $e->getMessage());
+}
+
 ?>
